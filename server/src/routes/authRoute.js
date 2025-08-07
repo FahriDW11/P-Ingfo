d577168c169d48f97d8cb3352e6baa7c95fc5566
FahriDW11 <fahriwicaksono011@gmail.com>
2025-08-07 23:39:10 +0700

    update auth route

--- server/src/routes/authRoute.js
+++ server/src/routes/authRoute.js
@@ -1,9 +1,13 @@
 import express from "express";
-import { getUserToken, addUser } from "../controllers/authController.js";
+import { getUserToken, addUser, changePassword, updateUser, deleteUser } from "../controllers/authController.js";
 
 const router = express.Router();
 
 router.post("/login", getUserToken);
 router.post("/register", addUser);
+
+router.put("/password/:id", verifyToken, changePassword);
+router.put("/:id", verifyToken, checkRole("admin"), updateUser);
+router.delete("/:id", verifyToken, checkRole("admin"), deleteUser);
 
 export default router;
